7f92b783aeb1a3471b78075ac9d42545212df286
prateek mahindra <prateekmahindra9@gmail.com>
2021-07-01 22:41:51 -0400

    Added serialize and deserialize functions

--- service/models.py
+++ service/models.py
@@ -56,3 +56,60 @@ class Promotion(db.Model):
     start_date = db.Column(db.DateTime(), nullable=False)
     end_date = db.Column(db.DateTime(), nullable=False)
     status = db.Column(db.String(63), nullable=True)
+
+
+
+    def serialize(self):
+        """Serializes a Pet into a dictionary"""
+        return {
+            "id": self.id,
+            "title": self.title,
+            "promotion_type": self.promotion_type,
+            "start_date": self.start_date,
+            "end_date": self.end_date, 
+            "status": self.status, 
+        }
+
+    def deserialize(self, data):
+        """
+        Deserializes a Pet from a dictionary
+
+        :param data: a dictionary of attributes
+        :type data: dict
+
+        :return: a reference to self
+        :rtype: Pet
+
+        """
+        try:
+            self.title = data["title"]
+            self.promotion_type = data["promotion_type"]
+            self.start_date= data["available"]
+            self.end_date = data["end_date"]  
+            self.status = data["status"] 
+        except KeyError as error:
+            raise DataValidationError("Invalid promotion: missing " + error.args[0])
+        except TypeError as error:
+            raise DataValidationError(
+                "Invalid promotion: body of request contained bad or no data"
+            )
+        return self
+
+    ##################################################
+    # CLASS METHODS
+    ##################################################
+
+    @classmethod
+    def init_db(cls, app):
+        """Initializes the database session
+
+        :param app: the Flask app
+        :type data: Flask
+
+        """
+        logger.info("Initializing database")
+        cls.app = app
+        # This is where we initialize SQLAlchemy from the Flask app
+        db.init_app(app)
+        app.app_context().push()
+        db.create_all()  # make our sqlalchemy tables
